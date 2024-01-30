a42dfc0c77ec61c070e10309acedfec6e2340033
John Louie Navales <104504427+johnlouie09@users.noreply.github.com>
2024-01-30 13:03:03 +0800

    change the aboutpage content

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,6 @@
 <template>
     <h1 class="mb-4">About</h1>
-    <p class="mb-3">About me? Hmm wala lang pa mekus mekus lang btw kumain ka naba?</p>
+    <p class="mb-3">About me? A lazy developer that was eager to learn some web development stuffs.</p>
 
 </template>
 
